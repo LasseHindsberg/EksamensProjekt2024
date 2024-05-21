5f1faa4118fee63f3027c80b14e758f871029b8c
LasseHindsberg <lassehindsberg@gmail.com>
2024-05-21 13:30:24 +0200

    added new components

---
+++ src/components/LocalDataSensorFetch.vue
@@ -0,0 +1,27 @@
+<script>
+import Axios from 'axios'
+
+export default {
+    setup() {
+        const fetchData = async () => {
+            try {
+                const response = await.axious.get('Azure Database API Link')
+            .then(response) => {
+                console.log (response.data)                
+            }}
+             catch (error) { 
+                console.error('error fetching data: ', error);
+            }
+                
+            }
+        }
+    }
+</script>
+<template>
+    <h1> Local Data Sensor &middot; Raspberry Pi </h1>
+    <button @click="fetchData">Fetch Latest Data</button>
+    <div class="dataContainer">
+        <p> Time: {{ time }} </p>
+
+</template>
+
---
+++ src/components/OpenWeatherApi.vue
@@ -0,0 +1,59 @@
+<script setup>
+import Axios from 'axios';
+import { ref } from 'vue';
+
+// Import environment variables
+const API_KEY = import.meta.env.VITE_APP_OPENWEATHERMAP_API_KEY;
+// Create a reactive variable to store the weather data
+let weatherData = ref([]);
+
+// Fetch the weather data from the OpenWeatherMap API
+const fetchData = async () => {
+    try {
+        const response = await Axios.get('https://api.openweathermap.org/data/2.5/forecast', {
+            params: {
+                q: 'roskilde,dk',
+                appid: API_KEY,
+                units: 'metric',
+                cnt: 5
+            }
+        });
+        
+        // Process the response data
+        const { list } = response.data;
+        const data = list.map(item => ({
+            time: item.dt_txt,
+            temperature: item.main.temp,
+            humidity: item.main.humidity,
+            weather: item.weather[0].description
+        }));
+
+        // Update the data in the component
+        weatherData.value = data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+// Fetch the data when the component is mounted
+fetchData();
+</script>
+
+
+<template>
+    <div>
+        <h1>OpenWeatherMap API</h1>
+      <ul v-for="(data,index) in weatherData" :key="index">
+        <h3> Time: {{ data.time }}</h3>
+        <li>
+          <p>Temperature: {{ data.temperature }}</p>
+        </li>
+        <li>
+          <p>Humidity: {{ data.humidity }}</p>
+        </li>
+        <li>
+          <p>Weather: {{ data.weather }}</p>
+        </li>
+        <br>
+      </ul>
+    </div>
+  </template>
