1324be929621b7de3c027c49ee77a222d5f70a21
LasseHindsberg <lassehindsberg@gmail.com>
2024-05-22 14:16:35 +0200

    function to calculate weather

--- src/components/LocalDataApi.vue
+++ src/components/LocalDataApi.vue
@@ -14,22 +14,38 @@ Axios.get('/src/data/data.json')
     .catch(error => {
         console.error('Error fetching data:', error);
     });
-    
+    // Function to calculate the weather
+    function calculateWeather(temperature, humidity) {
+        if (temperature > 25 && humidity > 70) {
+            return 'Hot and humid';
+        } else if (temperature > 25 && humidity <= 70) {
+            return 'Hot';
+        } else if (temperature <= 25 && humidity > 70) {
+            return 'Humid';
+        } else {
+            return 'Moderate';
+        }
+    }
+
+    // Call the calculateWeather function for each item in the dataList
+    dataList.value.forEach(item => {
+        item.weather = calculateWeather(item.temperature, item.humidity);
+    });
 </script>
 
 <template>
     <div class="localWeatherContainer">
         <h1>Local Sensor</h1>
-        <ul v-for="(item, index) in dataList" :key="index">
+        <ul class="localDataItem" v-for="(item, index) in dataList" :key="index">
+                <h3>Time: {{ item.time }}</h3>
             <li>
                 <p>Temperature: {{  item.temperature }}°C </p>
             </li>
-            <li>
-                <p>Time: {{ item.time }}</p>
-            </li>
+
             <li>
                 <p>Humidity: {{ item.humidity }}%</p>
             </li>
+            <br>
         </ul>
     </div>
 </template>
