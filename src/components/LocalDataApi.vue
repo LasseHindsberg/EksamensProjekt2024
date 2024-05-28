c5f0b948b57fcc3de6d4071edc023fb0faaf86ef
Fr0styy <lassehindsberg@gmail.com>
2024-05-28 05:12:13 +0200

    added classes

--- src/components/LocalDataApi.vue
+++ src/components/LocalDataApi.vue
@@ -1,54 +1,51 @@
 <script setup>
-import { ref } from 'vue';
-import Axios from 'axios';
+import { ref } from "vue";
+import Axios from "axios";
 
 // Create a reactive variable to store the data
 const dataList = ref([]);
 
 // Fetch data from local JSON file
 
-Axios.get('/src/data/data.json')
-    .then(response => {
-        dataList.value = response.data;
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+Axios.get("/src/data/data.json")
+	.then((response) => {
+		dataList.value = response.data;
+	})
+	.catch((error) => {
+		console.error("Error fetching data:", error);
+	});
 
-    // NEEDS DOING !!!!!!!!!
-    // Function to calculate the probability of accidents based on temperature and humidity
-    function calculateProbability(temperature, humidity) {
-        if (temperature > 25 && humidity > 70) {
-            return 'Low chance of accidents, High temperature and humidity';
-        } else if (temperature > 25 && humidity <= 70) {
-            return 'low chance of accidents, High temperature and low humidity';
-        } else if (temperature <= 25 && humidity > 70) {
-            return 'Humid';
-        } else {
-            return 'Low chance of accident';
-        }
-    }
-
+// NEEDS DOING !!!!!!!!!
+// Function to calculate the probability of accidents based on temperature and humidity
+function calculateProbability(temperature, humidity) {
+	if (temperature > 25 && humidity > 70) {
+		return "Low chance of accidents, High temperature and humidity";
+	} else if (temperature > 25 && humidity <= 70) {
+		return "low chance of accidents, High temperature and low humidity";
+	} else if (temperature <= 25 && humidity > 70) {
+		return "Humid";
+	} else {
+		return "Low chance of accident";
+	}
+}
 </script>
 
 <template>
-    <div class="localWeatherContainer">
-        <h1>Local Sensor</h1>
-        <ul class="localDataItem" v-for="(item, index) in dataList" :key="index">
-                <h3>Time: {{ item.time }}</h3>
-            <li>
-                <p>Temperature: {{  item.temperature }}°C </p>
-            </li>
+	<div class="localWeatherContainer">
+		<h1>Local Sensor</h1>
+		<ul class="localDataItem" v-for="(item, index) in dataList" :key="index">
+			<h3 class="localTime">Time: {{ item.time }}</h3>
+			<li>
+				<p class="localTemperature">Temperature: {{ item.temperature }}°C</p>
+			</li>
 
-            <li>
-                <p>Humidity: {{ item.humidity }}%</p>
-            </li>
-            <li>
-                <p> Chance of accidents: {{ calculateProbability(item.temperature, item.humidity)}}</p>
-            </li>
-            <br>
-        </ul>
-    </div>
+			<li>
+				<p class="localHumidity">Humidity: {{ item.humidity }}%</p>
+			</li>
+			<li>
+				<p class="localAccidentChance">Chance of accidents: {{ calculateProbability(item.temperature, item.humidity) }}</p>
+			</li>
+			<br />
+		</ul>
+	</div>
 </template>
-
-
--- src/components/OpenWeatherApi.vue
+++ src/components/OpenWeatherApi.vue
@@ -1,6 +1,6 @@
 <script setup>
-import Axios from 'axios';
-import { ref } from 'vue';
+import Axios from "axios";
+import { ref } from "vue";
 
 // Import environment variables
 const API_KEY = import.meta.env.VITE_APP_OPENWEATHERMAP_API_KEY;
@@ -9,53 +9,51 @@ let weatherData = ref([]);
 
 // Fetch the weather data from the OpenWeatherMap API
 const fetchData = async () => {
-    try {
-        const response = await Axios.get('https://api.openweathermap.org/data/2.5/forecast', {
-            params: {
-                q: 'roskilde,dk',
-                appid: API_KEY,
-                units: 'metric',
-                cnt: 5
-            }
-        });
-        
-        // Process the response data
-        const { list } = response.data;
-        const data = list.map(item => ({
-            time: item.dt_txt,
-            temperature: item.main.temp,
-            humidity: item.main.humidity,
-            weather: item.weather[0].description
-        }));
+	try {
+		const response = await Axios.get("https://api.openweathermap.org/data/2.5/forecast", {
+			params: {
+				q: "roskilde,dk",
+				appid: API_KEY,
+				units: "metric",
+				cnt: 5,
+			},
+		});
 
-        // Update the data in the component
-        weatherData.value = data;
-    } catch (error) {
-        console.error(error);
-    }
+		// Process the response data
+		const { list } = response.data;
+		const data = list.map((item) => ({
+			time: item.dt_txt,
+			temperature: item.main.temp,
+			humidity: item.main.humidity,
+			weather: item.weather[0].description,
+		}));
+
+		// Update the data in the component
+		weatherData.value = data;
+	} catch (error) {
+		console.error(error);
+	}
 };
 // Fetch the data when the component is mounted
 fetchData();
 </script>
 
-
 <template>
-    <div class="remoteWeatherContainer">
-        <h1>OpenWeatherMap API</h1>
-        
-        
-      <ul v-for="(data,index) in weatherData" :key="index">
-        <h3 class="RemoteTime"> Time: {{ data.time }}</h3>
-        <li>
-          <p class="RemoteTemperature">Temperature: {{ data.temperature }}</p>
-        </li>
-        <li>
-          <p class="RemoteHumidity">Humidity: {{ data.humidity }}</p>
-        </li>
-        <li>
-          <p class="RemoteWeather">Weather: {{ data.weather }}</p>
-        </li>
-        <br>
-      </ul>
-    </div>
-  </template>
+	<div class="remoteWeatherContainer">
+		<h1>OpenWeatherMap API</h1>
+
+		<ul class="remoteDataItem" v-for="(data, index) in weatherData" :key="index">
+			<h3 class="RemoteTime">Time: {{ data.time }}</h3>
+			<li>
+				<p class="RemoteTemperature">Temperature: {{ data.temperature }}</p>
+			</li>
+			<li>
+				<p class="RemoteHumidity">Humidity: {{ data.humidity }}</p>
+			</li>
+			<li>
+				<p class="RemoteWeather">Weather: {{ data.weather }}</p>
+			</li>
+			<br />
+		</ul>
+	</div>
+</template>
